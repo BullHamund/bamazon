372a749095006cd6c91ac877bd9df0b7d8bcb4bd
Bill Hammond <willmhammond@gmail.com>
2019-09-14 23:34:40 -0700

    removed password from js file

--- customer.js
+++ customer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: '3306',
     user: "root",
-    password: ".!.KPdA",
+    password: "",
     database: "bamazon"
   });
 
@@ -15,4 +15,3 @@ connection.connect(function(err) {
     console.log("you are connected!")
 })
 
-// make table function
